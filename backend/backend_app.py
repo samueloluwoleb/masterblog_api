55e035edd48896304dece2a011aace7d62b11fc3
Samuel Oluwole <samueloluwole2005@gmail.com>
2023-07-04 13:24:18 -0400

    working app before css for styling

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -1,19 +1,225 @@
-from flask import Flask, jsonify
+from json import JSONDecodeError
+
+from flask import Flask, jsonify, request
 from flask_cors import CORS
+from datetime import datetime
+import json
 
 app = Flask(__name__)
 CORS(app)  # This will enable CORS for all routes
 
-POSTS = [
-    {"id": 1, "title": "First post", "content": "This is the first post."},
-    {"id": 2, "title": "Second post", "content": "This is the second post."},
-]
+
+def load_data(file_path):
+    """
+        Loads the database.json file into the application
+    :param file_path:
+    :return:
+    """
+    with open(file_path, "r") as handle:
+        return json.load(handle)
+
+
+@app.route('/api/posts', methods=['POST'])
+def add_posts():
+    new_post = request.get_json()
+    title = new_post.get('title')
+    content = new_post.get('content')
+    author = new_post.get('author')
+    date = new_post.get('date')
+
+    if content == '':
+        return jsonify({"error": "Post content not found"}), 400
+    if author == '':
+        return jsonify({"error": "Post author not found"}), 400
+    if title == '':
+        return jsonify({"error": "Post author not found"}), 400
+    if date != f"{datetime.today().date()}":
+        date = f"{datetime.today().date()}"
+        new_post['date'] = date
+    try:
+        blog_posts = load_data('posts.json')
+    except JSONDecodeError:
+        return jsonify({"message": "Oops, there is a problem. It is not you, we promise !!!"}), 500
+
+    # Generate a new ID for the book
+    new_id = max(post['id'] for post in blog_posts) + 1
+    new_post['id'] = new_id
+    new_post['likes'] = 0
+
+    # Add the new book to our list
+    blog_posts.append(new_post)
+    json_data = json.dumps(blog_posts)
+
+    with open("posts.json", "w") as fileobj:
+        fileobj.write(json_data)
+
+    # Return the new book data to the client
+    return jsonify(new_post), 201
 
 
 @app.route('/api/posts', methods=['GET'])
 def get_posts():
-    return jsonify(POSTS)
+    sort = request.args.get('sort', '')
+    direction = request.args.get('direction')
+    try:
+        blog_posts = load_data('posts.json')
+    except JSONDecodeError:
+        return jsonify({"message": "Oops, there is a problem. It is not you, we promise !!!"}), 500
+
+    if sort == '':
+        return jsonify(blog_posts)
+
+    if sort == 'title' and direction == 'asc':
+        sorted_posts = sorted(blog_posts, key=lambda x: x['title'])
+    elif sort == 'title' and direction == 'desc':
+        sorted_posts = sorted(blog_posts, key=lambda x: x['title'], reverse=True)
+    elif sort == 'content' and direction == 'asc':
+        sorted_posts = sorted(blog_posts, key=lambda x: x['content'])
+    elif sort == 'content' and direction == 'desc':
+        sorted_posts = sorted(blog_posts, key=lambda x: x['content'], reverse=True)
+    elif sort == 'author' and direction == 'asc':
+        sorted_posts = sorted(blog_posts, key=lambda x: x['author'])
+    elif sort == 'author' and direction == 'desc':
+        sorted_posts = sorted(blog_posts, key=lambda x: x['author'], reverse=True)
+    elif sort == 'date' and direction == 'asc':
+        sorted_posts = sorted(blog_posts, key=lambda x: datetime.strptime(x['date'], '%Y-%m-%d').date())
+    elif sort == 'date' and direction == 'desc':
+        sorted_posts = sorted(blog_posts, key=lambda x: datetime.strptime(x['date'], '%Y-%m-%d').date(), reverse=True)
+    else:
+        return jsonify(blog_posts)
+
+    return jsonify(sorted_posts)
+
+
+@app.route('/api/posts/<int:post_id>', methods=['DELETE'])
+def delete(post_id):
+    try:
+        blog_posts = load_data('posts.json')
+    except JSONDecodeError:
+        return jsonify({"message": "Oops, there is a problem. It is not you, we promise !!!"}), 500
+    for count, post in enumerate(blog_posts):
+        if post['id'] == post_id:
+            del blog_posts[count]
+            json_data = json.dumps(blog_posts)
+
+            with open("posts.json", "w") as fileobj:
+                fileobj.write(json_data)
+            return jsonify({"message": f"Post with id {post_id} has been deleted successfully."})
+
+    return jsonify({"error": "No post with the provided id"}), 400
+
+
+@app.route('/api/posts/<int:post_id>', methods=['PUT'])
+def update(post_id):
+    """
+        Navigates to the update.html webpage, gets the update info from user using a form,
+        updates the database with the new information and loads the index.html page to view the changes
+    :param post_id:
+    :return:
+    """
+    new_post = request.get_json()
+    new_title = new_post.get('title')
+    new_content = new_post.get('content')
+    new_date = new_post.get('date')
+
+    try:
+        blog_posts = load_data('posts.json')
+    except JSONDecodeError:
+        return jsonify({"message": "Oops, there is a problem. It is not you, we promise !!!"}), 500
+    post = None
+    for post_data in blog_posts:
+        if post_data['id'] == post_id:
+            post = post_data
+    if post is None:
+        return jsonify({"error": "No post with the provided id"}), 400
+
+    if new_title == '':
+        new_title = post.get('title')
+    if new_content == '':
+        new_content = post.get('content')
+    if new_date != f"{datetime.today().date()}":
+        new_date = f"{datetime.today().date()}"
+
+    post['title'] = new_title
+    post['content'] = new_content
+    post['date'] = new_date
+
+    json_data = json.dumps(blog_posts)
+    with open("posts.json", "w") as fileobj:
+        fileobj.write(json_data)
+
+    return jsonify(post)
+
+
+@app.route('/api/posts/search')
+def search():
+    try:
+        blog_posts = load_data('posts.json')
+    except JSONDecodeError:
+        return jsonify({"message": "Oops, there is a problem. It is not you, we promise !!!"}), 500
+
+    search_word = request.args.get('search-word')
+    if search_word == '':
+        return jsonify(blog_posts)
+
+    list_of_posts = []
+    for post in blog_posts:
+        if search_word in post.get('title'):
+            list_of_posts.append(post)
+    for post in blog_posts:
+        if search_word in post.get('author'):
+            list_of_posts.append(post)
+    for post in blog_posts:
+        if search_word in post.get('content'):
+            list_of_posts.append(post)
+    for post in blog_posts:
+        if search_word in post.get('date'):
+            list_of_posts.append(post)
+    return jsonify(list_of_posts)
+
+    # key_word = ""
+    # search_word = ""
+    # search_query_list = ['author', 'title', 'date', 'content']
+    # for word in search_query_list:
+    #     if request.args.get(word, "") == "":
+    #         continue
+    #     else:
+    #         search_word = request.args.get(word)
+    #         key_word = word
+    #
+    # if search_word == "":
+    #     return jsonify({"error": f"Your - query does not match any post"}), 400
+    # else:
+    #     posts = [post for post in blog_posts if search_word in post.get(key_word)]
+    # if not posts:
+    #     return jsonify({"error": "Your search query does not match any post"}), 400
+    # else:
+    #     return jsonify(posts)
+
+
+@app.route('/api/posts/like/<int:post_id>')
+def like(post_id):
+    """
+       Increase the like count by 1, updates the blogpost database and loads the index.html page to view the changes
+    :param post_id:
+    :return:
+    """
+    try:
+        blog_posts = load_data('posts.json')
+    except JSONDecodeError:
+        return jsonify({"message": "Oops, there is a problem. It is not you, we promise !!!"}), 500
+
+    for post_data in blog_posts:
+        if post_data['id'] == post_id:
+            likes = post_data['likes'] + 1
+            post_data['likes'] = likes
+
+            json_data = json.dumps(blog_posts)
+            with open("posts.json", "w") as fileobj:
+                fileobj.write(json_data)
+            return jsonify(blog_posts)
 
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5002, debug=True)
+
--- frontend/static/main.js
+++ frontend/static/main.js
@@ -1,4 +1,13 @@
-// Function that runs once the window is fully loaded
+//Function that runs once the window is fully loaded
+var dropDownSelValue = "default";
+
+var selectElement = document.getElementById('sorting');
+selectElement.addEventListener('change', function() {
+    dropDownSelValue = handleSelectChange();
+    console.log('Selected value assigned:', dropDownSelValue);
+    loadPosts();
+});
+
 window.onload = function() {
     // Attempt to retrieve the API base URL from the local storage
     var savedBaseUrl = localStorage.getItem('apiBaseUrl');
@@ -9,6 +18,38 @@ window.onload = function() {
     }
 }
 
+function handleSelectChange() {
+    // Retrieve the selected option
+    var selectedOption = selectElement.options[selectElement.selectedIndex];
+    // Retrieve the value of the selected option
+    var selectedValue = selectedOption.value;
+
+    return selectedValue;
+}
+
+function renderTemplate(datas){
+    // Once the data is ready, we can use it
+    // Clear out the post container first
+    const postContainer = document.getElementById('post-container');
+    postContainer.innerHTML = '';
+
+    // For each post in the response, create a new post element and add it to the page
+    datas.forEach(post => {
+        const postDiv = document.createElement('div');
+        postDiv.className = 'post';
+        postDiv.innerHTML = `<h1>${post.author}</h1><h2>${post.title}</h2><p>${post.content}</p><p>${post.date}</p>
+        <button onclick="deletePost(${post.id})">Delete-eeeeeeeeee</button> <div class = "upBtn"><button onclick="showUpdateFields(${post.id})">U</button></div>
+        <input type="button" onclick="like(${post.id})" value="Like - ${post.likes}"/>
+        <div class="update-fields" id="update-fields-${post.id}" style="display: none;">
+            <input type="text" id="post-title-${post.id}" placeholder="Enter New Post Title">
+            <input type="text" id="post-content-${post.id}" placeholder="Enter New Post Content">
+            <input type="text" id="post-date-${post.id}" placeholder="*Enter New Post Date - YYYY-MM-DD" pattern="\d{4}-\d{2}-\d{2}" style="width: 310px;">
+            <button onclick="submit(${post.id})">Submit-tttt</button>
+        </div>`;
+        postContainer.appendChild(postDiv);
+    });
+}
+
 // Function to fetch all the posts from the API and display them on the page
 function loadPosts() {
     // Retrieve the base URL from the input field and save it to local storage
@@ -16,42 +57,113 @@ function loadPosts() {
     localStorage.setItem('apiBaseUrl', baseUrl);
 
     // Use the Fetch API to send a GET request to the /posts endpoint
+    if (dropDownSelValue == "default") {
     fetch(baseUrl + '/posts')
         .then(response => response.json())  // Parse the JSON data from the response
         .then(data => {  // Once the data is ready, we can use it
-            // Clear out the post container first
-            const postContainer = document.getElementById('post-container');
-            postContainer.innerHTML = '';
-
-            // For each post in the response, create a new post element and add it to the page
-            data.forEach(post => {
-                const postDiv = document.createElement('div');
-                postDiv.className = 'post';
-                postDiv.innerHTML = `<h2>${post.title}</h2><p>${post.content}</p>
-                <button onclick="deletePost(${post.id})">Delete</button>`;
-                postContainer.appendChild(postDiv);
-            });
+            renderTemplate(data);
         })
         .catch(error => console.error('Error:', error));  // If an error occurs, log it to the console
-}
+    }
+    if (dropDownSelValue == "author_asc") {
+    fetch(baseUrl + '/posts' + '?sort=author&direction=asc')
+        .then(response => response.json())  // Parse the JSON data from the response
+        .then(data => {  // Once the data is ready, we can use it
+            renderTemplate(data);
+        })
+        .catch(error => console.error('Error:', error));  // If an error occurs, log it to the console
+    }
+    if (dropDownSelValue == "author_desc") {
+    fetch(baseUrl + '/posts' + '?sort=author&direction=desc')
+        .then(response => response.json())  // Parse the JSON data from the response
+        .then(data => {  // Once the data is ready, we can use it
+            renderTemplate(data);
+        })
+        .catch(error => console.error('Error:', error));  // If an error occurs, log it to the console
+    }
+    if (dropDownSelValue == "title_asc") {
+    fetch(baseUrl + '/posts' + '?sort=title&direction=asc')
+        .then(response => response.json())  // Parse the JSON data from the response
+        .then(data => {  // Once the data is ready, we can use it
+            renderTemplate(data);
+        })
+        .catch(error => console.error('Error:', error));  // If an error occurs, log it to the console
+    }
+    if (dropDownSelValue == "title_desc") {
+    fetch(baseUrl + '/posts' + '?sort=title&direction=desc')
+        .then(response => response.json())  // Parse the JSON data from the response
+        .then(data => {  // Once the data is ready, we can use it
+            renderTemplate(data);
+        })
+        .catch(error => console.error('Error:', error));  // If an error occurs, log it to the console
+    }
+    if (dropDownSelValue == "content_asc") {
+    fetch(baseUrl + '/posts' + '?sort=content&direction=asc')
+        .then(response => response.json())  // Parse the JSON data from the response
+        .then(data => {  // Once the data is ready, we can use it
+            renderTemplate(data);
+        })
+        .catch(error => console.error('Error:', error));  // If an error occurs, log it to the console
+    }
+    if (dropDownSelValue == "content_desc") {
+    fetch(baseUrl + '/posts' + '?sort=content&direction=desc')
+        .then(response => response.json())  // Parse the JSON data from the response
+        .then(data => {  // Once the data is ready, we can use it
+            renderTemplate(data);
+        })
+        .catch(error => console.error('Error:', error));  // If an error occurs, log it to the console
+    }
+    if (dropDownSelValue == "date_asc") {
+    fetch(baseUrl + '/posts' + '?sort=date&direction=asc')
+        .then(response => response.json())  // Parse the JSON data from the response
+        .then(data => {  // Once the data is ready, we can use it
+            renderTemplate(data);
+        })
+        .catch(error => console.error('Error:', error));  // If an error occurs, log it to the console
+    }
+    if (dropDownSelValue == "date_desc") {
+    fetch(baseUrl + '/posts' + '?sort=date&direction=desc')
+        .then(response => response.json())  // Parse the JSON data from the response
+        .then(data => {  // Once the data is ready, we can use it
+            renderTemplate(data);
+        })
+        .catch(error => console.error('Error:', error));  // If an error occurs, log it to the console
+    }
 
+}
 // Function to send a POST request to the API to add a new post
 function addPost() {
     // Retrieve the values from the input fields
     var baseUrl = document.getElementById('api-base-url').value;
     var postTitle = document.getElementById('post-title').value;
     var postContent = document.getElementById('post-content').value;
+    var postAuthor = document.getElementById('post-author').value;
+    var postDateInput = document.getElementById('post-date');
+    var postDate = postDateInput.value;
+
+  // Check if the date is in the correct format (YYYY-MM-DD)
+  var dateRegex = /^\d{4}-\d{2}-\d{2}$/;
+  if (!dateRegex.test(postDate)) {
+    alert('Invalid date format. Please enter a date in the format YYYY-MM-DD.');
+    postDateInput.focus();
+    postDateInput.value = '';
+    return;
+  }
 
     // Use the Fetch API to send a POST request to the /posts endpoint
     fetch(baseUrl + '/posts', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ title: postTitle, content: postContent })
+        body: JSON.stringify({ title: postTitle, content: postContent, author: postAuthor, date: postDate  })
     })
     .then(response => response.json())  // Parse the JSON data from the response
     .then(post => {
         console.log('Post added:', post);
         loadPosts(); // Reload the posts after adding a new one
+        document.getElementById('post-title').value = '';
+        document.getElementById('post-content').value = '';
+        document.getElementById('post-author').value = '';
+        document.getElementById('post-date').value = '';
     })
     .catch(error => console.error('Error:', error));  // If an error occurs, log it to the console
 }
@@ -69,4 +181,65 @@ function deletePost(postId) {
         loadPosts(); // Reload the posts after deleting one
     })
     .catch(error => console.error('Error:', error));  // If an error occurs, log it to the console
+}
+
+function showUpdateFields(postId) {
+  const updateFieldsDiv = document.getElementById(`update-fields-${postId}`);
+  updateFieldsDiv.style.display = 'block';
+}
+
+
+function submit(postId) {
+    var baseUrl = document.getElementById('api-base-url').value;
+    var postTitle = document.getElementById(`post-title-${postId}`).value;
+    var postContent = document.getElementById(`post-content-${postId}`).value;
+    var postDateInput = document.getElementById(`post-date-${postId}`);
+    var postDate = postDateInput.value;
+
+  // Check if the date is in the correct format (YYYY-MM-DD)
+    var dateRegex = /^\d{4}-\d{2}-\d{2}$/;
+    if (!dateRegex.test(postDate)) {
+      alert('Invalid date format. Please enter a date in the format YYYY-MM-DD.');
+      postDateInput.focus();
+      postDateInput.value = '';
+      return;
+  }
+
+    // Use the Fetch API to send a POST request to the /posts endpoint
+    fetch(baseUrl + '/posts/' + postId, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ title: postTitle, content: postContent, date: postDate })
+    })
+    .then(response => response.json())  // Parse the JSON data from the response
+    .then(post => {
+        console.log('Post added:', post);
+        loadPosts(); // Reload the posts after adding a new one
+    })
+    .catch(error => console.error('Error:', error));  // If an error occurs, log it to the console
+}
+
+function search(){
+    var baseUrl = document.getElementById('api-base-url').value;
+    var postSearch = document.getElementById('post-search').value;
+
+    // Use the Fetch API to send a POST request to the /posts endpoint
+    fetch(baseUrl + '/posts' + '/search' + `?search-word=${postSearch}`)
+        .then(response => response.json())  // Parse the JSON data from the response
+        .then(data => {  // Once the data is ready, we can use it
+            renderTemplate(data);
+        })
+
+}
+
+function like(postId) {
+    var baseUrl = document.getElementById('api-base-url').value;
+
+    // Use the Fetch API to send a DELETE request to the specific post's endpoint
+    fetch(baseUrl + '/posts' + '/like/' + postId)
+        .then(response => response.json())  // Parse the JSON data from the response
+        .then(data => {  // Once the data is ready, we can use it
+            renderTemplate(data);
+        })
+        .catch(error => console.error('Error:', error));  // If an error occurs, log it to the console
 }
